e3e45cc99ac271dbe84db638be7499b877d8a5ac
Code_Blooded <90474550+Rishab260@users.noreply.github.com>
2024-09-28 10:50:41 +0530

    Update settings.py

--- tableocr/tableocr/settings.py
+++ tableocr/tableocr/settings.py
@@ -22,7 +22,7 @@ STATIC_DIR = path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4wqo*((sz@jar$3)2wsd191qs&2gvq)yzay$0qf6y1w&_*b^9g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
